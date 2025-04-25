9475de10e398a7fbec78660ffeb72350b78036ac
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2025-04-26 00:28:01 +0700

    add cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
+	adapter: cloudflare(),
 	integrations: [
 		starlight({
 			title: 'My Docs',
